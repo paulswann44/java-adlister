f140d2bd8df725e7fda5fc39d063ab7cfbf6d22c
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2023-02-08 21:44:16 -0600

    ...

---
+++ src/main/java/Config.java
@@ -0,0 +1,11 @@
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://localhost:3306/paul_adlister_db?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+    public String getUser() {
+        return "main";
+    }
+    public String getPassword() {
+        return "Password123!!";
+    }
+}
